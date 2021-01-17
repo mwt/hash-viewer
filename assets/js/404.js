a4ac0ca5b51b899780b01c2736b79e84f8e19fea
Matthew Thomas <matthew@mwt.me>
2021-01-17 00:45:21 -0600

    Add support for MathJaX

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,8 +1,20 @@
 function replaceText() {
     var hash = window.location.pathname.substring(1);
     var content = decodeURIComponent(escape(atob(hash)));
-    document.getElementById('content').innerHTML = marked(content);
-}
+    var contents = content.split('$$');
+    if (contents.length === 1) {
+        document.getElementById('content').innerHTML = marked(content);
+    } else {
+        contents[0] = marked(contents[0]);
+        for (let index = 2; index < contents.length; index++) {
+            if (index % 2 === 0) {
+                contents[index] = marked(contents[index]);
+            };
+        };
+        document.getElementById('content').innerHTML = contents.join('$$');
+    };
+    
+};
 
 replaceText();
 
