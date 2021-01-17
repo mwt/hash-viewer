955dd0771225cdec441b9ea09841e5096db35127
Matthew Thomas <matthew@mwt.me>
2021-01-17 10:33:34 -0600

    add comments and clean js

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,19 +1,24 @@
 function replaceText() {
+    /* get the base64 hash from the url */
     var hash = window.location.pathname.substring(1);
+    /* decode the hash into text */
     var content = decodeURIComponent(escape(atob(hash)));
+    /* split into math and markdown segments */
     var contents = content.split('$$');
     if (contents.length === 1) {
+        /* if there is no math, process it with marked and replace text in content div */
         document.getElementById('content').innerHTML = marked(content);
     } else {
-        contents[0] = marked(contents[0]);
-        for (let index = 2; index < contents.length; index++) {
+        /* else, process only even indices with marked so that math is untouched */
+        for (let index = 0; index < contents.length; index++) {
             if (index % 2 === 0) {
                 contents[index] = marked(contents[index]);
             };
         };
+        /* join again and replace text in content div */
         document.getElementById('content').innerHTML = contents.join('$$');
     };
-    
+
 };
 
 replaceText();
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,10 @@ function clearText() {
 }
 
 function submitText() {
+    /* get the value in the textarea */
     var myText = document.getElementById('markdown').value;
+    /* convert the value to a base64 hash */
     var myHash = btoa(unescape(encodeURIComponent(myText)));
+    /* redirect to hash page */
     window.location.href = `/${myHash}`;
 }
