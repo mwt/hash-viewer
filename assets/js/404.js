f94bacccbec90b3932d39fa5cacb9e11e3b029b1
Matthew Thomas <matthew@mwt.me>
2021-02-06 16:51:55 -0600

    Load MathJaX only when necessary

--- assets/js/404.js
+++ assets/js/404.js
@@ -9,7 +9,12 @@ function replaceText() {
         /* if there is no math, process it with marked and replace text in content div */
         document.getElementById('content').innerHTML = marked(content);
     } else {
-        /* else, process only even indices with marked so that math is untouched */
+        /* load MathJaX only when needed */
+        const jaxTag = document.createElement('script');
+        jaxTag.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js";
+        jaxTag.async = true;
+        document.body.appendChild(jaxTag);
+        /* process only even indices with marked so that math is untouched */
         for (let index = 0; index < contents.length; index++) {
             if (index % 2 === 0) {
                 contents[index] = marked(contents[index]);
@@ -18,7 +23,6 @@ function replaceText() {
         /* join again and replace text in content div */
         document.getElementById('content').innerHTML = contents.join('$$');
     };
-
 };
 
 replaceText();
